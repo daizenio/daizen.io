dfbba5f68e9140992173dd4b3b24aa7901c72a44
tom <t@daizen.io>
2023-05-24 18:44:02 +0200

    nv

--- scripts/create_specs.sh
+++ scripts/create_specs.sh
@@ -1,1 +1,8 @@
-./target/release/parachain-template-node build-spec --disable-default-bootnode >plain-parachain-chainspec.json
+./target/release/parachain-daizen build-spec --disable-default-bootnode >plain-parachain-chainspec.json
+./target/release/parachain-daizen build-spec --chain plain-parachain-chainspec.json --disable-default-bootnode --raw > raw-parachain-chainspec.json
+
+./target/release/parachain-daizen export-genesis-wasm --chain raw-parachain-chainspec.json para-2000-wasm
+
+./target/release/parachain-daizen  export-genesis-state --chain raw-parachain-chainspec.json para-2000-genesis-state
+
+
